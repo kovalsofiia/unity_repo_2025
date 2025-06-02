774c43e821dfc1a0d73f479d9a58065e8024eb0c
kovalsofiia <126290290+kovalsofiia@users.noreply.github.com>
2025-06-02 23:27:18 +0300

    Lab5 5/7

---
+++ Assets/Scripts/lab2/LoginManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoginManager : MonoBehaviour
+{
+    public InputField usernameInput;
+    public Button startButton;
+    public Text warningText;
+
+    void Start()
+    {
+        warningText.text = "";
+        startButton.onClick.AddListener(OnStartClicked);
+    }
+
+    public void OnStartClicked()
+    {
+        string username = usernameInput.text.Trim();
+
+        if (string.IsNullOrEmpty(username))
+        {
+            warningText.text = "Please enter your login.";
+        }
+        else
+        {
+            PlayerPrefs.SetString("username", username);
+            Debug.Log("Clicked");
+            SceneManager.LoadScene("MenuLab5");
+        }
+    }
+}
--- Assets/Scripts/lab2/TotalTimer.cs
+++ Assets/Scripts/lab2/TotalTimer.cs
@@ -6,7 +6,7 @@ public class TotalTimer : MonoBehaviour
 {
     private float elapsedTime = 0f;
     public Text timerText;
-    [SerializeField] private GameObject deathPanel;
+    public Text gameOverText;
 
     private bool isGameRunning = true;
 
@@ -14,8 +14,6 @@ public class TotalTimer : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        if (deathPanel != null)
-            deathPanel.SetActive(false);
         elapsedTime = 0f;
     }
 
@@ -29,19 +27,6 @@ public class TotalTimer : MonoBehaviour
             if (timerText != null)
             {
                 timerText.text = "Time: " + Mathf.RoundToInt(elapsedTime);
-            }
-
-            if (Input.GetKeyDown(KeyCode.Escape))
-            {
-                isGameRunning = false;
-                OnTimerEnd?.Invoke();
-
-                if (deathPanel != null)
-                    deathPanel.SetActive(false);
-                {
-                    deathPanel.gameObject.SetActive(true);
-                    Debug.Log("Game Over\nTotal Time: " + Mathf.RoundToInt(elapsedTime) + "s");
-                }
             }
         }
     }
